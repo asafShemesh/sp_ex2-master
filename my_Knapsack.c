5e7701761636956bf2dd1ab0656bd31816c5097c
asaf <asaf0604@gmail.com>
2024-02-11 22:56:35 +0200

    jifjf

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -1,17 +1,16 @@
 
 #include <stdio.h>
+#define bag_max_weight 20
+#define num_items 5
 
 
-int knapsack(int weights[], int values[], int result[]) {
-    int mat[6][21]={{0}};
-    for (int i = 0; i < 6; i++) {
-        for (int w = 0; w < 21; w++) {
-            if (i == 0 || w == 0)
-                mat[i][w] = 0;
-            else if (weights[i - 1] <= w) {
+int knapSack (int weights[], int values[] , int selected_bool[]) {
+    int mat[num_items+1][bag_max_weight+1]={{0}};
+    for (int i = 0; i < num_items+1; i++) {
+        for (int w = 0; w < bag_max_weight+1; w++) {
+            if (weights[i - 1] <= w) {
                 int include = values[i - 1] + mat[i - 1][w - weights[i - 1]];
                 int exclude = mat[i - 1][w];
-
                 if (include > exclude) {
                     mat[i][w] = include;
                 } else {
@@ -22,13 +21,13 @@ int knapsack(int weights[], int values[], int result[]) {
             }
         }
     int w=20;
-    int sum=mat[5][20];
-    for (size_t i = 5; i > 0 && sum>0 ; i--)
+    int susu=mat[num_items][bag_max_weight];
+    for (size_t i = 5; i > 0 && susu>0 ; i--)
     {
-       int was_added = sum != mat[i-1][w];
+       int was_added = susu != mat[i-1][w];
        if(was_added){
-        result[i-1]=1;
-        sum-=values[i-1];
+        selected_bool[i-1]=1;
+        susu-=values[i-1];
         w-=weights[i-1];
        }
        
@@ -39,15 +38,15 @@ int knapsack(int weights[], int values[], int result[]) {
 
     }
 
-    return mat[5][20];
+    return mat[num_items][bag_max_weight];
 }
 
 
 int main(){
-char items[5];
-int values[5];
-int weight[5];
-int result[5]={0};
+char items[num_items];
+int values[num_items];
+int weight[num_items];
+int result[num_items]={0};
 
 
     for (int i = 0; i < 4; i++)
@@ -64,11 +63,11 @@ int result[5]={0};
          scanf("%d",&weight[4]);
 
 
-    int solution = knapsack(weight, values, result);
+    int solution = knapSack(weight, values, result);
         printf("Maximum profit: %d\n", solution);
 
         printf("Selected items:");
-          for (int i = 0; i < 5; i++) {
+          for (int i = 0; i < num_items; i++) {
                 if (result[i] == 1) {
                  printf(" %c", items[i]);
               }
