2ad8710f6d8562d473cf0e78cda75f66b95ee92f
asaf <asaf0604@gmail.com>
2024-02-11 23:00:00 +0200

    aplly

--- my_mat.c
+++ my_mat.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <math.h>
 #define MAX_SIZE 10
-int fakeMatrix[10][10];
+int fakeMatrix[MAX_SIZE][MAX_SIZE];
 
 int function1(int mat[MAX_SIZE][MAX_SIZE], int size)
 {
@@ -20,19 +20,19 @@ int function1(int mat[MAX_SIZE][MAX_SIZE], int size)
 void floydWarshall(int mat[MAX_SIZE][MAX_SIZE])
 {
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < MAX_SIZE; i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < MAX_SIZE; j++)
         {
           fakeMatrix[i][j]=mat[i][j];
         }
     }
 //check it out
-    for (int k = 0; k < 10; k++)
+    for (int k = 0; k < MAX_SIZE; k++)
     {
-        for(int i = 0; i < 10; i++)
+        for(int i = 0; i < MAX_SIZE; i++)
         {
-            for(int j = 0; j < 10; j++)
+            for(int j = 0; j < MAX_SIZE; j++)
             {
                 if ( fakeMatrix[i][k] != 0 && fakeMatrix[k][j] != 0)
                 {
