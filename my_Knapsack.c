5f77592f69424814d1a499134dd81a70561a6271
asaf <asaf0604@gmail.com>
2024-02-11 18:06:34 +0200

    cfcf

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -1,46 +1,78 @@
 
 #include <stdio.h>
 
-char items[5];
-int values[5];
-int weight[5];
 
-
-int max(int a, int b) {
-    return (a > b) ? a : b;
-}
-
-int knapsack(int weights[], int values[]) {
-    int mat[6][21];
+int knapsack(int weights[], int values[], int result[]) {
+    int mat[6][21]={{0}};
     for (int i = 0; i < 6; i++) {
         for (int w = 0; w < 21; w++) {
             if (i == 0 || w == 0)
                 mat[i][w] = 0;
-            else if (weights[i - 1] <= w)
-                mat[i][w] = max(values[i - 1] + mat[i - 1][w - weights[i - 1]], mat[i - 1][w]);
-            else
+            else if (weights[i - 1] <= w) {
+                int include = values[i - 1] + mat[i - 1][w - weights[i - 1]];
+                int exclude = mat[i - 1][w];
+
+                if (include > exclude) {
+                    mat[i][w] = include;
+                } else {
+                    mat[i][w] = exclude;
+                }
+            } else {
                 mat[i][w] = mat[i - 1][w];
+            }
         }
+    int w=20;
+    int sum=mat[5][20];
+    for (size_t i = 5; i > 0 && sum>0 ; i--)
+    {
+       int was_added = sum != mat[i-1][w];
+       if(was_added){
+        result[i-1]=1;
+        sum-=values[i-1];
+        w-=weights[i-1];
+       }
+       
+    }
+    
+    
+
+
     }
 
     return mat[5][20];
 }
 
 
-
 int main(){
+char items[5];
+int values[5];
+int weight[5];
+int result[5]={0};
 
 
-    for (int i = 0; i <5 ; i++)
+    for (int i = 0; i < 4; i++)
       {
 
          scanf("%c",&items[i]);
          scanf("%d",&values[i]);
-         scanf("%d",&weight[i]);
+         scanf("%d ",&weight[i]);
 
     }
-    int result = knapsack(weight, values);
-    printf("Maximum profit: %d\n", result);
+
+         scanf("%c",&items[4]);
+         scanf("%d",&values[4]);
+         scanf("%d",&weight[4]);
+
+
+    int solution = knapsack(weight, values, result);
+        printf("Maximum profit: %d\n", solution);
+
+        printf("Selected items: ");
+          for (int i = 0; i < 5; i++) {
+                if (result[i] == 1) {
+                 printf("%c ", items[i]);
+              }
+         }
 
     return 0;
 
