fd54cf116f8329a17643522a8ef2e2ac35ca485b
asaf <asaf0604@gmail.com>
2024-02-11 16:17:36 +0200

    nji

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all:my_graph my_knapsack
+all:my_graph my_Knapsack
 
 my_graph: my_graph.o my_mat.o
 	gcc -Wall my_graph.o my_mat.o -o my_graph
@@ -6,13 +6,13 @@ my_graph: my_graph.o my_mat.o
 my_graph.o: my_graph.c my_mat.h
 	gcc -Wall -c my_graph.c 
 
-my_knapsack.o: my_knapsack.c
-	gcc -Wall -c my_knapsack.c 
+my_Knapsack.o: my_Knapsack.c
+	gcc -Wall -c my_Knapsack.c 
 
 my_mat.o: my_mat.c my_mat.h
 	gcc -Wall -c my_mat.c 
 
 clean:
-	rm *.o my_graph 
+	rm *.o my_graph my_Knapsack
 
 .PHONY : .clean all
--- my_Knapsack.c
+++ my_Knapsack.c
@@ -1,9 +1,31 @@
 
 #include <stdio.h>
 
-int items[5];
+char items[5];
 int values[5];
 int weight[5];
+
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int knapsack(int weights[], int values[]) {
+    int mat[6][21];
+    for (int i = 0; i < 6; i++) {
+        for (int w = 0; w < 21; w++) {
+            if (i == 0 || w == 0)
+                mat[i][w] = 0;
+            else if (weights[i - 1] <= w)
+                mat[i][w] = max(values[i - 1] + mat[i - 1][w - weights[i - 1]], mat[i - 1][w]);
+            else
+                mat[i][w] = mat[i - 1][w];
+        }
+    }
+
+    return mat[5][20];
+}
+
 
 
 int main(){
@@ -12,36 +34,14 @@ int main(){
     for (int i = 0; i <5 ; i++)
       {
 
-         scanf("%c",items[i]);
-         scanf("%d",values[i]);
-         scanf("%d",weight[i]);
+         scanf("%c",&items[i]);
+         scanf("%d",&values[i]);
+         scanf("%d",&weight[i]);
 
     }
-    int result = knapsack(capacity, weights, values, n)
-    printf("Maximum value that can be obtained is: %d\n", result);
+    int result = knapsack(weight, values);
+    printf("Maximum profit: %d\n", result);
 
     return 0;
 
-}
-
-
-int knapsack(int capacity, int weights[], int values[]) {
-    int dp[6][21];
-
-    for (int i = 0; i < 6; i++) {
-        for (int w = 0; w < 21; w++) {
-            if (i == 0 || w == 0)
-                dp[i][w] = 0;
-            else if (weights[i - 1] <= w)
-                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
-            else
-                dp[i][w] = dp[i - 1][w];
-        }
-    }
-
-    return dp[5][20];
-}
-
-int max(int a, int b) {
-    return (a > b) ? a : b;
 }
